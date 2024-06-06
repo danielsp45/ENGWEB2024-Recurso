9ceed38f436c52e794a75ce40886fd022c5ce0b0
daniel_sp <mail@danielpereira.xyz>
2024-06-06 14:54:15 +0100

    queries

--- ex1/app.js
+++ ex1/app.js
@@ -6,6 +6,15 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var mongoose = require("mongoose");
+var mongoDB = "mongodb://127.0.0.1/livros";
+mongoose.connect(mongoDB);
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "Erro de conexão ao MongoDB"));
+db.once("open", () => {
+  console.log("Conexão ao MongoDB realizada com sucesso");
+});
+
 var app = express();
 
 app.use(logger('dev'));
